077a93575b966eac1d9c27b04e7c20c999d05a3b
Fabio Melpignano <melpignano.fabio@gmail.com>
2020-10-10 22:51:59 +0000

    Adding logs (level to be reviewed).

--- src/main/java/org/truelayer/rest/json/ShakespeareanPokemonResource.java
+++ src/main/java/org/truelayer/rest/json/ShakespeareanPokemonResource.java
@@ -5,12 +5,17 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.jboss.logging.Logger;
+
 @Path("/resteasy/hello")
 public class ShakespeareanPokemonResource {
 
+	private static final Logger LOGGER = Logger.getLogger(ShakespeareanPokemonResource.class);
+	
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public String hello() {
+    	LOGGER.debug("Receieved Request");
         return "hello";
     }
 }
